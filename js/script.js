647fcd5d0c4f9a4419dff9a9477ed6d1e36f15c1
nazarii <nazariypodzihun@gmail.com>
2022-02-20 23:09:06 +0200

    test commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваше фамилия?', '');
+answers[2] = prompt('Ваш возраст?', '');
+
+document.write(answers);
